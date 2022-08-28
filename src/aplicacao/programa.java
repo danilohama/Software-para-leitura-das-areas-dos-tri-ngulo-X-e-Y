58f52b14b1a1dca70e2c70eae00abcc87e2ea11e
Your Name <danilo.ohamaa@gmail.com>
2022-08-27 23:44:28 -0300

    Atualização do software, Crianção de uma classe com três atributos para representar melhor o triângulo

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -3,6 +3,8 @@ package aplicacao;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entidade.triangulo;
+
 public class programa {
 
 	public static void main(String[] args) {
@@ -10,23 +12,25 @@ public class programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double xA,xB,xC,yA,yB,yC;
+        triangulo x , y;
+        x = new triangulo();
+        y = new triangulo();
 		
 	    System.out.println("Entre com o valor do triângulo X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Entre com o valor do triângulo Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c= sc.nextDouble();
 		
-		double p =(xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA)*(p - xB)*(p - xC));
+		double p =(x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a)*(p - x.b)*(p - x.c));
 	
-	    p =(yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA)*(p - yB)*(p - yC));
+	    p =(y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a)*(p - y.b)*(p - y.c));
 		
 		System.out.printf("valor do triângulo X: %.4f%n" , areaX);
 	 
---
+++ src/entidade/triangulo.java
@@ -0,0 +1,8 @@
+package entidade;
+
+public class triangulo { // atributo da classe entidade
+
+	public double a; //A,B e C são os amétodos do atributo triagulo
+	public double b;
+	public double c;
+}
