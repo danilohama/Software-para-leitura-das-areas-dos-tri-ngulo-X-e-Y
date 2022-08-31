3375128a3670868403c212cb1dc1dbb636bbd55a
Your Name <danilo.ohamaa@gmail.com>
2022-08-31 17:41:31 -0300

    Atualização e melhorias onde tirei as repetições dentro do código

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -2,7 +2,6 @@ package aplicacao;
 
 import java.util.Locale;
 import java.util.Scanner;
-
 import entidade.triangulo;
 
 public class programa {
@@ -26,11 +25,10 @@ public class programa {
 		y.b = sc.nextDouble();
 		y.c= sc.nextDouble();
 		
-		double p =(x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a)*(p - x.b)*(p - x.c));
-	
-	    p =(y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a)*(p - y.b)*(p - y.c));
+		
+		double areaX = x.area();
+	   double  areaY = y.area();
+	    
 		
 		System.out.printf("valor do triângulo X: %.4f%n" , areaX);
 	 
--- src/entidade/triangulo.java
+++ src/entidade/triangulo.java
@@ -2,7 +2,14 @@ package entidade;
 
 public class triangulo { // atributo da classe entidade
 
-	public double a; //A,B e C são os amétodos do atributo triagulo
+	public double a; //A,B e C são os métodos do atributo triagulo
 	public double b;
 	public double c;
+	
+	//Método e função
+	public double area() {
+	double p =(a + b + c) / 2.0;
+	double resultado = Math.sqrt(p*(p - a)*(p - b)*(p-c));
+	return resultado;
+}
 }
